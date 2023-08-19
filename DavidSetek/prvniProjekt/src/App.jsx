c5a42f8204ed265b44b9379299b4c738f2e648b6
LipusM <mlipus007@gmail.com>
2023-08-19 10:40:41 +0200

    Vytváření komponenty Book

---
+++ DavidSetek/prvniProjekt/src/App.jsx
@@ -0,0 +1,17 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import Book from "./components/Book";
+
+const App = () => {
+  return (
+    <>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </>
+  );
+};
+
+export default App;
---
+++ DavidSetek/prvniProjekt/src/components/Book/index.jsx
@@ -0,0 +1,13 @@
+import "./Book.scss"
+
+const Book = () => {
+
+    return (
+        <section>
+            <h2 className="bookHeading">Název knihy</h2>
+        </section>
+    )
+
+}
+
+export default Book
