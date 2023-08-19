96bcf83aac8dcc6dbb9f6f5cb2cbe35180fe1c25
LipusM <mlipus007@gmail.com>
2023-08-19 11:44:50 +0200

    Import dat

--- DavidSetek/prvniProjekt/src/App.jsx
+++ DavidSetek/prvniProjekt/src/App.jsx
@@ -1,9 +1,29 @@
 import viteLogo from "/vite.svg";
-import "./App.css";
+import "./app.scss";
 
 import Book from "./components/Book";
 
 const App = () => {
+
+  const books = [
+    {
+      id: 1,
+      image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-kamen-mudrcu-9788000061917.jpg.webp",
+      title: "Harry Potter a Kámen mudrců"
+    },
+    {
+      id: 2,
+      image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/333420609/10.jpg.webp",
+      title: "Harry Potter a Tajemná komnata"
+    },
+    {
+      id: 3,
+      image: "https://www.knihydobrovsky.cz/thumbs/book-list/mod_eshop/produkty/h/harry-potter-a-vezen-z-azkabanu-9788000063393.jpg.webp",
+      title: "Harry Potter a Vězeň z Azkabanu"
+    },
+  ]
+
+
   return (
     <section className="allBooks">
       <Book />
