49f94dedc0ce2e88401fdd1305c1ac175f9a416b
LipusM <mlipus007@gmail.com>
2023-08-20 17:30:26 +0200

    Úprava komponenty Example

--- DavidSetek/druhyProjekt/src/components/Example/index.jsx
+++ DavidSetek/druhyProjekt/src/components/Example/index.jsx
@@ -4,34 +4,41 @@ import "./style.scss";
 import { useState } from "react";
 
 const Example = () => {
-
   const [title, setTitle] = useState("Můj první title");
 
   const buttonHandler1 = () => {
-    setTitle("Nový text titulku.")
+    setTitle("Nový text titulku.");
   };
 
   const buttonHandler2 = () => {
-    setTitle("Opět změněný text!")
-  }
+    setTitle("Opět změněný text!");
+  };
 
   const deleteAll = () => {
-      setTitle("")
-  }
+    setTitle("");
+  };
 
-  const [buttonTxt, setButtonTxt] = useState("Původní text tlačítka")
+  const [buttonTxt, setButtonTxt] = useState("Původní text tlačítka");
 
   const changeButtonTxt = () => {
-      setButtonTxt("Změněný text tlačítka.")
-  }
+    setButtonTxt("Změněný text tlačítka.");
+  };
 
   return (
-    <section>
-      <h2>{title}</h2>
-      <button type="button" onClick={buttonHandler1}>Změnit title</button>
-      <button type="button" onClick={buttonHandler2}>Opět změnit title</button>
-      <button type="button" onClick={deleteAll}>Vymazat titulek</button>
-      <button type="button" onClick={changeButtonTxt}>{buttonTxt}</button>
+    <section className="exampleSection">
+        <h2>{title}</h2>
+        <button type="button" onClick={buttonHandler1}>
+          Změnit title
+        </button>
+        <button type="button" onClick={buttonHandler2}>
+          Opět změnit title
+        </button>
+        <button type="button" onClick={deleteAll}>
+          Vymazat titulek
+        </button>
+        <button type="button" onClick={changeButtonTxt}>
+          {buttonTxt}
+        </button>
     </section>
   );
 };
