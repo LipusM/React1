66a1a320ea4759bb38a5dcbbda1570a1065a185e
LipusM <mlipus007@gmail.com>
2023-08-21 18:32:11 +0200

    Trening fci map a filter

--- DavidSetek/tretiProjekt/src/App.jsx
+++ DavidSetek/tretiProjekt/src/App.jsx
@@ -1,15 +1,13 @@
+const c = console.log.bind(document)
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './app.scss'
+import data from './data'
 
 const App = () => {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-
-    </>
+    <h1>Test</h1>
   )
 }
 
