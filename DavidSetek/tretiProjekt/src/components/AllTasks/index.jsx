e8f24004bd857c4f5c7734332366f7fc06b398db
LipusM <mlipus007@gmail.com>
2023-08-22 19:24:30 +0200

    Přidání vlastnosti ukazující počet zbývajících úkolů

--- DavidSetek/tretiProjekt/src/components/AllTasks/index.jsx
+++ DavidSetek/tretiProjekt/src/components/AllTasks/index.jsx
@@ -5,36 +5,40 @@ import "./style.scss";
 import data from "../../data";
 
 const AllTasks = () => {
+  const [myTask, setMyTask] = useState(data);
 
-    const [myTask, setMyTask] = useState(data)
+  const tasksHandler = (id) => {
+    const filteredTasks = myTask.filter((oneTask) => {
+      if (oneTask.id !== id) {
+        return oneTask.id;
+      }
+    });
 
-    const tasksHandler = (id) => {
-        const filteredTasks = myTask.filter( oneTask => {
-            if(oneTask.id !== id){
-                return oneTask.id
-            }
-        } )
+    c(filteredTasks.length);
+    setMyTask(filteredTasks);
+  };
 
-        setMyTask(filteredTasks)
-    }
-
-    const allDeleteHandler = () => {
-      setMyTask([])
-    }
+  const allDeleteHandler = () => {
+    setMyTask([]);
+  };
 
   return (
     <div className="tasks">
-      {
-          myTask.map( oneTask => {
-            const {id, name} = oneTask
+      {myTask.map((oneTask) => {
+        const { id, name } = oneTask;
 
-            return <div className="one-task" key={id}>
-                <h4>{name}</h4>
-                <button onClick={ () => tasksHandler(id)}>Vymazat</button>
-            </div>
-          })
-      }
-      <button className="main-button" onClick={allDeleteHandler}>Vše vymazat</button>
+        return (
+          <div className="one-task" key={id}>
+            <h4>{name}</h4>
+            <button onClick={() => tasksHandler(id)}>Vymazat</button>
+          </div>
+        );
+      })}
+
+      <div className="btnAndnmb">
+        <button className="main-button" onClick={allDeleteHandler}>Vše vymazat</button>
+        <p>Počet zbývajících úkolů: {myTask.length}</p>
+      </div>
     </div>
   );
 };
