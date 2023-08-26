580027b1e1cc0e8bbf33d5349ac9a977d2a6186a
LipusM <mlipus007@gmail.com>
2023-08-26 12:26:29 +0200

    Nefunkční ter. operátor

--- cviceniTeorie/cviceniPodmineneZobrazeni/src/App.jsx
+++ cviceniTeorie/cviceniPodmineneZobrazeni/src/App.jsx
@@ -1,10 +1,29 @@
 import './app.scss'
 
+import ShoppingList from './components/ShoppingItem'
+
 function App() {
+
+  const toShop = [
+    {
+      item: "Brambory",
+      amount: 3,
+      unit: "kg",
+      bought: true,
+      id: 1,
+    },
+    {
+      item: "Rajčata",
+      amount: 0.5,
+      unit: "kg",
+      bought: true,
+      id: 2,
+    },
+  ]
 
   return (
     <>
-
+      {toShop.map( oneItem => < ShoppingList item={oneItem.item} amount={oneItem.amount} unit={oneItem.unit} key={oneItem.id}/> )}
     </>
   )
 }
--- cviceniTeorie/cviceniPodmineneZobrazeni/src/components/ShoppingItem/index.jsx
+++ cviceniTeorie/cviceniPodmineneZobrazeni/src/components/ShoppingItem/index.jsx
@@ -2,10 +2,14 @@ const c = console.log.bind(document)
 
 import "./style.scss"
 
-const ShoppingList = () => {
+const ShoppingList = (props) => {
+
+    const {item, amount, unit, bought, id} = props
 
     return (
-        <></>
+        <div key={id}>
+            <p><b>Položka:</b> {bought ? "✔" : "x"} {item} {amount} {unit}</p>
+        </div>
     )
 
 }
