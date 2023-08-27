68998d220bdb2f0ad6ab77b6aa35651eb869daf7
LipusM <mlipus007@gmail.com>
2023-08-27 10:12:07 +0200

    Úprava jsx

--- cviceniTeorie/cviceniNastavovaniCssTrid/src/components/Product/index.jsx
+++ cviceniTeorie/cviceniNastavovaniCssTrid/src/components/Product/index.jsx
@@ -1,13 +1,17 @@
-const c = console.log.bind(document)
+const c = console.log.bind(document);
 
-import "./style.scss"
+import "./style.scss";
 
-const Product = () => {
+const Product = ({name, description, price, isFavorite}) => {
 
-    return (
-        <></>
-    )
+  return (
+    <div className="product">
+      <h2 className="product__name">{name} ⭐</h2>
+      <p className="product__description">{description}</p>
+      <p className="product__price">{price} Kč</p>
+    </div>
+  );
 
-}
+};
 
-export default Product
+export default Product;
