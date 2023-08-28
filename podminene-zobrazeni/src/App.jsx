d4cac9784fca200147b3c427af3e0c7262aa09b5
LipusM <mlipus007@gmail.com>
2023-08-28 17:34:09 +0200

    Opakování

--- podminene-zobrazeni/src/App.jsx
+++ podminene-zobrazeni/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   
   return (
     <>
-      <UserInfo name="Jarda" surname="Vomáčka" age={28} isLoggedIn={false} />
+      <UserInfo name="Jarda" surname="Vomáčka" age={28}/>
     </>
   )
 
--- podminene-zobrazeni/src/components/UserInfo/index.jsx
+++ podminene-zobrazeni/src/components/UserInfo/index.jsx
@@ -1,43 +1,14 @@
-const c = console.log.bind(document);
+const c = console.log.bind(document)
 
-import "./style.scss";
-import classnames from "classnames"
+import "./style.scss"
 
-const UserInfo = (props) => {
-  const { name, surname, age, isLoggedIn } = props;
-
-/*   let userInfoClass = "userinfo"
-  if(isLoggedIn){
-    userInfoClass += "loggedIn"
-  } else {
-    userInfoClass += "notLoggedIn"
-  } */
-
-/*   let info
-  if(isLoggedIn){
-      info = <p>JE přihlášený</p>
-  } else {
-      info = <p>NENÍ přihlášený</p>
-  } */
-
-  return (
-    <div className={ classnames("userinfo", 
-    {loggedIn: true, notLoggedIng: true}) 
-    }>
+const UserInfo = ({name, surname, age}) => {
+  return ( 
+    <div className="userinfo">
       <h2>Informace o uživateli</h2>
-      <p>
-        {name} {surname} vek {age} let
-      </p>
-
-{/*       {info}
-      {isLoggedIn ? <p>JE</p> : <p>NENI</p>}
-      <p>Uživatel {isLoggedIn ? "JE" : "NENI"} přihlášený.</p> */}
-
-{/*       {isLoggedIn ? <p>JE</p> : null}
-      {!isLoggedIn && <p>NENI</p>} */}
-
+      <p>{name} {surname}, vek {age} let</p>
     </div>
-  );
-};
-
-export default UserInfo;
+   );
+}
+ 
+export default UserInfo
