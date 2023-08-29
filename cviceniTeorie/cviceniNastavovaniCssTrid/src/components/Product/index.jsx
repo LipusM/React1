b2b1291ba61da3dd3c0f648998a5dd3a106e32b4
LipusM <mlipus007@gmail.com>
2023-08-29 19:22:49 +0200

    Vylepšení nastavování tříd

--- cviceniTeorie/cviceniNastavovaniCssTrid/src/components/Product/index.jsx
+++ cviceniTeorie/cviceniNastavovaniCssTrid/src/components/Product/index.jsx
@@ -5,9 +5,17 @@ import "./style.scss";
 
 const Product = ({name, description, price, isFavorite}) => {
 
-  return (
+/*   return (
     <div className="product">
       <h2 className={isFavorite ? "product__name product--favorite" : "product__name"}>{name}</h2>
+      <p className="product__description">{description}</p>
+      <p className="product__price">{price} Kč</p>
+    </div>
+  ); */
+
+  return (
+    <div className={classNames("product", {"product--favorite": isFavorite})}>
+      {isFavorite ? <h2>{name} ⭐</h2> : <h2>{name}</h2>}
       <p className="product__description">{description}</p>
       <p className="product__price">{price} Kč</p>
     </div>
