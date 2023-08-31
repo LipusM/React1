24687d075c23c13babc0111f8b02e3266232c28e
LipusM <mlipus007@gmail.com>
2023-08-31 18:32:13 +0200

    Cvičení 6 done

--- cviceniTeorie/cviceni-udalosti-stav-main/src/components/Cviceni6/index.jsx
+++ cviceniTeorie/cviceni-udalosti-stav-main/src/components/Cviceni6/index.jsx
@@ -5,14 +5,28 @@
 - když bude stavová hodnota true, odstavec textu bude vidět a na tlačítku bude text "Schovat text"
 - na tlačítko přidejte událost reagující na kliknutí, která bude přepínat hodnotu stavu true/false
 */
+const c = console.log.bind(document)
+
+import { useState } from "react";
 
 function Cviceni6() {
+
+	const [text, setText] = useState(false)
+	const handleClick = () => {
+		setText(!text)
+	}
+
 	return (
 		<div className="card">
 			<h3><span>6</span> Rozbalovací nabídka</h3>
 
-			<button>Schovej/zobraz text</button>
-			<p>Tento ostavec textu se po kliknutí na tlačítko bude buď zobrazovat nebo schovávat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo explicabo quasi placeat deserunt impedit quas nisi sed exercitationem saepe?</p>
+			<button onClick={handleClick}> {!text ? "Zobraz text" : "Schovat text"} </button>
+			<p>
+				{
+				text && 
+				"Tento ostavec textu se po kliknutí na tlačítko bude buď zobrazovat nebo schovávat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti illo explicabo quasi placeat deserunt impedit quas nisi sed exercitationem saepe?"
+				}
+			</p>
 		</div>
 	);
 }
