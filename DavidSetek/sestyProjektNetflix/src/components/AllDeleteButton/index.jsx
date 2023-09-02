6a24c2f7613ed574462a7a47d5b49741f4d9c612
LipusM <mlipus007@gmail.com>
2023-09-02 16:10:14 +0200

    AllDeleteButton

---
+++ DavidSetek/sestyProjektNetflix/src/components/AllDeleteButton/index.jsx
@@ -0,0 +1,13 @@
+const c = console.log.bind(document)
+
+import "./style.scss"
+
+const AllDeleteButton = () => {
+
+    return (
+        <button>Vymaž všechny filmy</button>
+    )
+
+}
+
+export default AllDeleteButton
