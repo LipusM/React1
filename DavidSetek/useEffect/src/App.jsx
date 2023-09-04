5f9c0c0c7d4a2b8958d69d4b868838c6207d9ac7
LipusM <mlipus007@gmail.com>
2023-09-04 19:05:05 +0200

    Pokročilejší useEffect()

--- DavidSetek/useEffect/src/App.jsx
+++ DavidSetek/useEffect/src/App.jsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
+const c = console.log.bind(document)
+
+import {useState, useEffect} from "react"
 import './app.scss'
 
-function App() {
+const App = () => {
 
+  {/* Přes useState */}
+/*   const [number, setNumber] = useState(0)
+  const [click, setClick] = useState(0)
+  const handleClick = () => {
+    setNumber(number + 1)
+    setClick(click + 1)
+  } */
+
+
+  {/* Přes useEffect */}
+  const [number, setNumber] = useState(0)
+
+  useEffect( () => {
+    /* const button = document.querySelector(".btn") */
+    /* button.textContent = `Klikni: ${click}` */
+
+    const button = document.querySelector(".btn")
+
+    if(number >= 1){
+      button.textContent = `Počet kliknutí: ${number}`
+    }
+
+  }, [])
 
   return (
     <>
+      <h1>Já jsem return</h1>
+      <p>{number}</p>
 
+      {/* Přes useState */}
+      {/*  <button onClick={ handleClick }>Počet kliknutí: {click}</button> */} 
+
+      {/* Přes useEffect */}
+      <button className="btn" onClick={ () => setNumber(number + 1) }>Klikni na tlačítko!</button> 
     </>
   )
 }
