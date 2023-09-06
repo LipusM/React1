d2523dd945af1a6c4f7cecf06fd3d66fc5951099
LipusM <mlipus007@gmail.com>
2023-09-06 18:55:47 +0200

    Drobné úpravy

--- cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/Episode/index.jsx
+++ cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/Episode/index.jsx
@@ -1,27 +1,24 @@
-const c = console.log.bind(document)
+const c = console.log.bind(document);
 
+import "./style.scss";
+import episodes from "./data";
 
-import "./style.scss"
-import episodes from "./data"
+const Episode = ({ num, title, guest }) => {
+  return (
+    <>
+      {episodes.map((episode) => (
+        <div key={episode.num} className="episodes-list">
+          <div className="episode">
+            <div className="episode__num">{episode.num}</div>
+            <div className="episode__body">
+              <div className="episode__title">{episode.title}</div>
+              <div className="episode__guest">host: {episode.guest}</div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
-const Episode = ({num, title, guest}) => {
-
-    return (
-        <>
-            {episodes.map(episode => 
-                <div key={episode.num} className="episodes-list">
-                <div className="episode">
-                    <div className="episode__num">{episode.num}</div>
-                    <div className="episode__body">
-                        <div className="episode__title">{episode.title}</div>
-                        <div className="episode__guest">host: {episode.guest}</div>
-                    </div>
-                </div>
-                </div>
-                )}
-        </>
-    )
-
-}
-
-export default Episode
+export default Episode;
