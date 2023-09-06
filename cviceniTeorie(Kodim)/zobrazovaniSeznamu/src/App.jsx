017df8b350a8acc8099f5eb3875ea75582dd838c
LipusM <mlipus007@gmail.com>
2023-09-06 18:52:19 +0200

    Komponenta Episode

--- cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/App.jsx
+++ cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/App.jsx
@@ -5,7 +5,9 @@ import OpakovaniMap from './components/OpakovaniMap'
 import SeznamyJSX from './components/SeznamyJSX'
 import SlozitejsiKomponenty from './components/SlozitejsiKomponenty'
 import CviceniSeznamy from './components/CviceniSeznamy'
+
 import Episode from './components/Episode'
+import episodes from './components/Episode/data'
 
 
 const App = () => {
--- cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/Episode/index.jsx
+++ cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/Episode/index.jsx
@@ -7,23 +7,19 @@ import episodes from "./data"
 const Episode = ({num, title, guest}) => {
 
     return (
-            <div className="episodes-list">
+        <>
+            {episodes.map(episode => 
+                <div key={episode.num} className="episodes-list">
                 <div className="episode">
-                    <div className="episode__num">127</div>
+                    <div className="episode__num">{episode.num}</div>
                     <div className="episode__body">
-                        <div className="episode__title">Hledání plyšového Yettiho</div>
-                        <div className="episode__guest">host: Vojtěch Ryba</div>
+                        <div className="episode__title">{episode.title}</div>
+                        <div className="episode__guest">host: {episode.guest}</div>
                     </div>
                 </div>
-
-                <div className="episode">
-                    <div className="episode__num">128</div>
-                    <div className="episode__body">
-                        <div className="episode__title">Moderní hrachová kaše</div>
-                        <div className="episode__guest">host: Kamila Štancová</div>
-                    </div>
                 </div>
-            </div>
+                )}
+        </>
     )
 
 }
