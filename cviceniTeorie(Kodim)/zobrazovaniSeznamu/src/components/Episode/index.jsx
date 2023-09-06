49033a598d63ba20f8ab36935770f7adc0880070
LipusM <mlipus007@gmail.com>
2023-09-06 18:40:26 +0200

    Stylování

--- cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/Episode/index.jsx
+++ cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/Episode/index.jsx
@@ -1,11 +1,29 @@
 const c = console.log.bind(document)
 
-import "./style.scss"
 
-const Episode = () => {
+import "./style.scss"
+import episodes from "./data"
+
+const Episode = ({num, title, guest}) => {
 
     return (
-        <></>
+            <div className="episodes-list">
+                <div className="episode">
+                    <div className="episode__num">127</div>
+                    <div className="episode__body">
+                        <div className="episode__title">Hledání plyšového Yettiho</div>
+                        <div className="episode__guest">host: Vojtěch Ryba</div>
+                    </div>
+                </div>
+
+                <div className="episode">
+                    <div className="episode__num">128</div>
+                    <div className="episode__body">
+                        <div className="episode__title">Moderní hrachová kaše</div>
+                        <div className="episode__guest">host: Kamila Štancová</div>
+                    </div>
+                </div>
+            </div>
     )
 
 }
