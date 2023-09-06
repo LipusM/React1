16da9ae08ed34e2e9b88c1803be0e319495c8c8c
LipusM <mlipus007@gmail.com>
2023-09-06 22:56:56 +0200

    Komponenta BarevnePalety

--- cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/App.jsx
+++ cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/App.jsx
@@ -6,7 +6,13 @@ import SeznamyJSX from './components/SeznamyJSX'
 import SlozitejsiKomponenty from './components/SlozitejsiKomponenty'
 import CviceniSeznamy from './components/CviceniSeznamy'
 import Episode from './components/Episode'
+
 import BarevnePalety from './components/barevnePalety'
+import palettes from './components/barevnePalety/data'
+import mimosaRetreat from "../src/components/BarevnePalety/img/mimosa-retreat.jpg"
+import nightSky from "../src/components/BarevnePalety/img/night-sky.jpg"
+import oceanWaves from "../src/components/BarevnePalety/img/ocean-waves.jpg"
+import santorini from "../src/components/BarevnePalety/img/santorini.jpg"
 
 
 
@@ -14,7 +20,14 @@ const App = () => {
 
   return(
     <>
-      <BarevnePalety />
+      {palettes.map(palette => < BarevnePalety name={palette.name} attributionName={palette.attribution.name} 
+      attributionURL={palette.attribution.url} id={palette.colors[0]} img={palette.image} 
+      color1={palette.colors[0]}
+      color2={palette.colors[1]}
+      color3={palette.colors[2]}
+      color4={palette.colors[3]}
+      color5={palette.colors[4]}
+      /> )}
     </>
   )
 
--- cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/BarevnePalety/index.jsx
+++ cviceniTeorie(Kodim)/zobrazovaniSeznamu/src/components/BarevnePalety/index.jsx
@@ -1,11 +1,47 @@
 const c = console.log.bind(document)
 
 import "./style.scss"
+import palettes from "./data"
 
-const BarevnePalety = () => {
+import mimosaRetreat from "./img/mimosa-retreat.jpg"
+import nightSky from "./img/night-sky.jpg"
+import oceanWaves from "./img/ocean-waves.jpg"
+import santorini from "./img/santorini.jpg"
+
+const BarevnePalety = ({name, attributionName, attributionURL, img, color1, color2, color3, color4, color5, direction, description, id}) => {
 
     return (
-        <></>
+        <>
+    <div className="container">
+      <header>
+        <h1>Barevné palety</h1>
+      </header>
+      <main>
+        <div key={id} className="palette">
+          <div className="palette-scheme palette-scheme--vertical">
+            <img className="scheme-image" src={img} alt={name} />
+            <div className="scheme-colors">
+              <div className="scheme-color" style={{ backgroundColor: color1 }} >{color1}</div>
+              <div className="scheme-color" style={{ backgroundColor: color2 }} >{color2}</div>
+              <div className="scheme-color" style={{ backgroundColor: color3 }} >{color3}</div>
+              <div className="scheme-color" style={{ backgroundColor: color4 }} >{color4}</div>
+              <div className="scheme-color" style={{ backgroundColor: color5 }} >{color5}</div>
+            </div>
+          </div>
+          <div className="palette-info">
+            <h2>{name}</h2>
+            <p>{description}</p>
+
+            <p>Photo by <a href={attributionURL} target="_blank">{attributionName}</a>.</p>
+          </div>
+        </div>
+        
+      </main>
+      <footer>
+        <p>Czechitas, Digitální akademie: Web</p>
+      </footer>
+    </div>
+        </>
     )
 
 }
