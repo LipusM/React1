243d2517387f5df7dfb9cc0cdcc040fa19578722
LipusM <mlipus007@gmail.com>
2023-09-09 15:17:11 +0200

    PragueTime komponenta dokončena

--- cviceniTeorie(Kodim)/formularovePrvkyEfekty/src/components/PragueTime/index.jsx
+++ cviceniTeorie(Kodim)/formularovePrvkyEfekty/src/components/PragueTime/index.jsx
@@ -5,30 +5,40 @@ import "./style.scss";
 
 const PragueTime = () => {
 
-    const [datetime, setDatetime] = useState("")
+    //Pro vykreslení správného času
+  const [datetime, setDatetime] = useState("");
 
-    useEffect(() => {
-        
-        fetch("https://worldtimeapi.org/api/timezone/Europe/Prague")
-        .then(response => response.json())
-        .then(data => {
-            let mainTime = data.datetime
-            mainTime = mainTime.slice(0, 19)
+  //Pro výběr časové zóny (ovlivňuje vykres. správného času)
+  const [timezone, setTimezone] = useState("Europe/Prague")
 
-            setDatetime(mainTime)
-        })
-    }, [])
+  useEffect(() => {
+    fetch(`https://worldtimeapi.org/api/timezone/${timezone}`)
+      .then((response) => response.json())
+      .then((data) => {
+        let mainTime = data.datetime;
+        mainTime = mainTime.slice(0, 19);
 
-    return (
-        <>
-            <h1>Pražský čas</h1>
-            <div>
-                Dnešní datum: {datetime} <br />
+        setDatetime(mainTime);
+      });
+  });
 
-            </div>
-        </>
-    )
+  return (
+    <>
+      <h1>Pražský čas</h1>
+      <div>Dnešní datum: {datetime}</div>
+      <label>
+          Vyberte zónu:
+        <select value={timezone} onChange={ e => setTimezone(e.target.value) }>
+          <option value="America/New_York">New York</option>
+          <option value="Europe/London">Londýn</option>
+          <option value="Europe/Moscow">Moskva</option>
+          <option value="Europe/Prague">Praha</option>
+          <option value="Asia/Hong_Kong">Hong Kong</option>
+          <option value="Asia/Jerusalem">Jeruzalém</option>
+        </select>
+      </label>
+    </>
+  );
+};
 
-}
-
-export default PragueTime
+export default PragueTime;
