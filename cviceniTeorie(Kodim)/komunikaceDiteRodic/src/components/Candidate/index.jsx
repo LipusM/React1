67171b653e2918b69322de317b55323e71102383
LipusM <mlipus007@gmail.com>
2023-09-09 22:00:13 +0200

    Úprava komponenty Candidate.

--- cviceniTeorie(Kodim)/komunikaceDiteRodic/src/components/Candidate/index.jsx
+++ cviceniTeorie(Kodim)/komunikaceDiteRodic/src/components/Candidate/index.jsx
@@ -3,11 +3,11 @@ const c = console.log.bind(document)
 import React from 'react';
 import './style.scss';
 
-const Candidate = ({ name, avatar }) => (
+const Candidate = ({ name, avatar, onVote }) => (
   <div className="candidate">
     <h3 className="candidate__name">{name}</h3>
     <img className="candidate__avatar" src={avatar} />
-    <button className="btn-vote">Vybrat</button>
+    <button className="btn-vote" onClick={ () => onVote(name) }>Vybrat</button>
   </div>
 );
 
