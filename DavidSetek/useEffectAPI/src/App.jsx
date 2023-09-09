32f79462f6c8560aaf94355377b5da4e6adb5fae
LipusM <mlipus007@gmail.com>
2023-09-09 12:44:18 +0200

    Import UseEffectAPI2 komponenty

--- DavidSetek/useEffectAPI/src/App.jsx
+++ DavidSetek/useEffectAPI/src/App.jsx
@@ -3,12 +3,12 @@ import { useState, useEffect } from 'react'
 import './app.scss'
 
 import UseEffectAPI from './components/UseEffectAPI'
-
+import UseEffectAPI2 from './components/UseEffectAPI2'
 const App = () => {
 
   return (
     <>
-
+      <UseEffectAPI2 />
     </>
   )
 }
