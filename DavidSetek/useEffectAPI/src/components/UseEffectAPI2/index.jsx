dd495e045e221453054e8708ba3033f94b00acd3
LipusM <mlipus007@gmail.com>
2023-09-09 12:44:28 +0200

    Založení komponenty

---
+++ DavidSetek/useEffectAPI/src/components/UseEffectAPI2/index.jsx
@@ -0,0 +1,14 @@
+const c = console.log.bind(document)
+
+import { useState, useEffect } from 'react'
+import "./style.scss"
+
+const UseEffectAPI2 = () => {
+
+    return (
+        <></>
+    )
+
+}
+
+export default UseEffectAPI2
