e0bb82dbeef8c502e204be4deda184913ee0c4e7
LipusM <mlipus007@gmail.com>
2023-09-09 12:51:03 +0200

    Provedení fetch způsobem, jakým jsme se to učili na Czechitas.

--- DavidSetek/useEffectAPI/src/components/UseEffectAPI2/index.jsx
+++ DavidSetek/useEffectAPI/src/components/UseEffectAPI2/index.jsx
@@ -5,8 +5,20 @@ import "./style.scss"
 
 const UseEffectAPI2 = () => {
 
+    const [quote, setQuote] = useState("")
+
+    useEffect( () => {
+
+        fetch("https://api.kanye.rest/")
+        .then(response => response.json())
+        .then(data => setQuote(data.quote) )
+
+    }, [])
+
     return (
-        <></>
+        <>
+            <h1>{quote}</h1>
+        </>
     )
 
 }
