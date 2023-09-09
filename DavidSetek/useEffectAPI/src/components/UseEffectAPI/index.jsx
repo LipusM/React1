db31ae78f40b4285ddb8bbc493ade401a966e77b
LipusM <mlipus007@gmail.com>
2023-09-09 12:44:00 +0200

    Přidání useEffect

--- DavidSetek/useEffectAPI/src/components/UseEffectAPI/index.jsx
+++ DavidSetek/useEffectAPI/src/components/UseEffectAPI/index.jsx
@@ -1,12 +1,31 @@
 const c = console.log.bind(document)
 
-import "./style.scss"
 import { useState, useEffect } from 'react'
+import "./style.scss"
+
 
 const UseEffectAPI = () => {
 
+    const [quote, setQuote] = useState("Výchozí text")
+
+    const url = "https://api.kanye.rest/"
+
+    const getQuote = async  () => {
+        const response = await fetch(url)
+        const data = await response.json()
+
+        const finalQuote = data["quote"]
+        setQuote(finalQuote)
+    }
+    
+    useEffect( () => {
+        getQuote()
+    }, [])
+
     return (
-        <></>
+        <>
+            <h1>{quote}</h1>
+        </>
     )
 
 }
