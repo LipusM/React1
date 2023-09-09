b68cd5b08cf679793fbffa5fbfecab7f8ea4249d
LipusM <mlipus007@gmail.com>
2023-09-09 21:31:06 +0200

    Oprava načtení obrázků

--- cviceniTeorie(Kodim)/komunikaceDiteRodic/src/components/AllCandidates/index.jsx
+++ cviceniTeorie(Kodim)/komunikaceDiteRodic/src/components/AllCandidates/index.jsx
@@ -5,15 +5,20 @@ import { render } from 'react-dom';
 import Candidate from '../Candidate/index.jsx';
 import './style.scss';
 
+import avatar1 from "../../assets/candidate01.png"
+import avatar2 from '../../assets/candidate02.png'
+import avatar3 from '../../assets/candidate03.png'
+import avatar4 from '../../assets/candidate04.png'
+
 const AllCandidates = () => {
     const [candidates, setCandidates] = useState([]);
     const [president, setPresident] = useState(null);
   
     useEffect(() => setCandidates([
-      { name: "Ferdinand Mravenec", avatar: '../../assets/candidate01.png'},
-      { name: "Markéta Smetana", avatar: '/assets/candidate02.png' },
-      { name: "Beáta Skočdopolová", avatar: '/assets/candidate03.png' },
-      { name: "Lubomír Poňuchálek", avatar: '/assets/candidate04.png' },
+      { name: "Ferdinand Mravenec", avatar: avatar1},
+      { name: "Markéta Smetana", avatar: avatar2 },
+      { name: "Beáta Skočdopolová", avatar: avatar3 },
+      { name: "Lubomír Poňuchálek", avatar: avatar4 },
     ]), []);
     
     return (
