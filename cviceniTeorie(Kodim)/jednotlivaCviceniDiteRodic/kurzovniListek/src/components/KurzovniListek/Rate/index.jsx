96dbd0f70a14a2b5bcf86a90730d621ec5a48658
LipusM <mlipus007@gmail.com>
2023-09-10 16:52:08 +0200

    Založení komponenty Rate

---
+++ cviceniTeorie(Kodim)/jednotlivaCviceniDiteRodic/kurzovniListek/src/components/KurzovniListek/Rate/index.jsx
@@ -0,0 +1,22 @@
+const c = console.log.bind(document);
+
+import React from "react";
+import "./style.scss";
+
+const currencies = {
+  USD: { CZK: 23.823 },
+  EUR: { CZK: 24.74 },
+  GBP: { CZK: 29.067 },
+};
+
+const Rate = () => {
+  return (
+    <div className="rate">
+      <div className="rate__currency">1 USD</div>
+      <div>=</div>
+      <div className="rate__value">22 CZK</div>
+    </div>
+  );
+};
+
+export default Rate;
