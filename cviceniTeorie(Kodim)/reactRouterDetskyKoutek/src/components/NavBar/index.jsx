91a58191c7de41b949b7d34a46c183e3f1ae67a5
LipusM <mlipus007@gmail.com>
2023-09-18 20:14:07 +0200

    .

--- cviceniTeorie(Kodim)/reactRouterDetskyKoutek/src/components/NavBar/index.jsx
+++ cviceniTeorie(Kodim)/reactRouterDetskyKoutek/src/components/NavBar/index.jsx
@@ -20,3 +20,28 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
+
+/* const c = console.log.bind(document);
+
+import { Link, Outlet, NavLink } from "react-router-dom";
+import "./style.scss";
+
+const NavBar = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <NavLink to="/" className={ ({isActive, isPending}) => { isActive ? "activeLink": "nonActiveLink"; isPending && "pending" }
+        }>Home</NavLink>
+          <NavLink to="/about" className={ ({isActive, isPending}) => { isActive ? "activeLink": "nonActiveLink"; isPending && "pending" } }>About</NavLink>
+          <NavLink to="/contact" className={ ({isActive, isPending}) => { isActive ? "activeLink": "nonActiveLink"; isPending && "pending" } }>Contact</NavLink>
+          <NavLink to="/pobocky" className={ ({isActive, isPending}) => { isActive ? "activeLink": "nonActiveLink"; isPending && "pending" } }>Pobočky</NavLink>
+        </nav>
+      </header>
+      <Outlet />
+    </>
+  );
+};
+
+export default NavBar; */
