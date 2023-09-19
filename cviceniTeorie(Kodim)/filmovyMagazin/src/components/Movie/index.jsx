ca454ed789b1e16f41451542f54ae4099537ed28
LipusM <mlipus007@gmail.com>
2023-09-19 22:26:56 +0200

    ..

--- cviceniTeorie(Kodim)/filmovyMagazin/src/components/Movie/index.jsx
+++ cviceniTeorie(Kodim)/filmovyMagazin/src/components/Movie/index.jsx
@@ -1,11 +1,13 @@
 const c = console.log.bind(document)
 
 import { useParams } from "react-router-dom"
+/* import { useState, useEffect } from "react" */
 
 import "./style.scss"
 import movies from "../../movieDatabase"
 
 const Movie = () => {
+
 
     const { id } = useParams()
     const selectedMovie = movies.find(chosenMovie=> chosenMovie.id === Number(id))
@@ -17,6 +19,11 @@ const Movie = () => {
             <p>Genre: {selectedMovie.genre}</p>
             <p>Rating: {selectedMovie.rating}</p>
             <p>Director: {selectedMovie.director}</p>
+            <p>Cast: 
+                {
+                selectedMovie.cast.map(actor => <span key={selectedMovie.id}> {actor.name} as: {actor.as};</span>)
+                }
+            </p>
         </div>
     )
 }
