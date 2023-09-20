362a3fa02024d779cccb7023a77caef0546f3c5f
LipusM <mlipus007@gmail.com>
2023-09-20 19:57:31 +0200

    ..

--- ziveLekce/efekty6L/src/components/Postavy/index.jsx
+++ ziveLekce/efekty6L/src/components/Postavy/index.jsx
@@ -10,20 +10,38 @@ const Postavy = () => {
     const [nacitani, setNacitani] = useState(true)
     const [postavy, setPostavy] = useState([])
 
-    useEffect( () => {
+    const [oci, setOci] = useState("")
 
+    useEffect( () => {
+        setNacitani(true)
         fetch(`https://swapi.dev/api/people`)
         .then(response => response.json())
         .then(data => {
-            setPostavy(data.results)
+            
+            if(oci === ""){
+                setPostavy(data.results)
+            } else {
+                setPostavy(data.results.filter(p => p.eye_color === oci))
+            }
+
             setNacitani(false)
         })
 
-    }, [])
+    }, [oci])
 
     return(
         <>
         <h2>Postavy</h2>
+
+        <hr />
+        <select value={oci} onChange={(e) => {setOci(e.target.value)} }>
+            <option value=""></option>
+            <option value="blue">Modré</option>
+            <option value="brown">Hnědé</option>
+            <option value="yellow">Žluté</option>
+            <option value="red">Červené</option>
+        </select>
+        <hr />
 
         { nacitani ?
         <p>Načítám data</p> 
